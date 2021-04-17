3ad2d3bf863b4402f1ae64bf8193f33f0fad63c7
hrudkouski <aprilshower19@gmail.com>
2021-04-17 15:03:16 +0300

    hw1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import style from './Message.module.css';
 
-function Message() {
+export type MessageTypeProps = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessageTypeProps) {
     return (
-        <div>
-
+        <div className={style.wrapper}>
+            <span className={style.avatar}>
+                <img src={props.avatar} alt="avatar"/>
+            </span>
+            <div className={style.messages}>
+                <div className={style.name}>
+                    {props.name}
+                </div>
+                <div className={style.message}>
+                    {props.message}
+                </div>
+                <div className={style.time}>
+                    {props.time}
+                </div>
+            </div>
+            <svg width="31" height="151">
+                <polygon points="5,135 115,5 225,135"
+                />
+            </svg>
         </div>
     )
 }
